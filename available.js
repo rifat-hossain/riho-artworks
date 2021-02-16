95a3308344d204ad8e8ba31200f89879448abe18
rifat-hossain <rifathossainayon@gmail.com>
2021-02-16 14:09:43 +0600

    completed search engine

--- available.js
+++ available.js
@@ -3,13 +3,42 @@ xhttp.open("GET", "data.json", true);
 xhttp.send(null);
 
 var nothing = true;
+var url = new URL(window.location.href);
+var q = decodeURIComponent(url.searchParams.get('q'));
+var low = parseInt(url.searchParams.get('low'));
+var high = parseInt(url.searchParams.get('high'));
 
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var json = JSON.parse(xhttp.response).posts;
         for(var i=0; i < json.length; i++){
             if(json[i].sold == false){
-                nothing == false;
+                //matching keywords
+                if(q != "" && q != null && q != "null"){
+                    document.getElementById("q").value = q;
+                    var keywords = q.split(" ");
+                    var pass = false;
+                    keywords.forEach(element => {
+                        if(json[i].title.toLowerCase().indexOf(element.toLowerCase()) > -1){
+                            pass = true;
+                        }
+                    });
+                    if(pass == false){
+                        console.log("Skipped by key");
+                        continue;
+                    }
+                }
+                //matching price
+                if(low != NaN && high != NaN && low <= high){
+                    document.getElementById("low").value = low;
+                    document.getElementById("high").value = high;
+                    if(json[i].price < low || json[i].price > high){
+                        console.log("Skipped by price");
+                        continue;
+                    }
+                }
+
+                nothing = false;
                 var body = "<div class=\"box text-center\">\
                 <h5>"+json[i].title+"</h5>\
                 <img src=\"images/"+json[i].img+"\" class=\"mx-auto img-fluid\">\
@@ -23,7 +52,7 @@ xhttp.onreadystatechange = function() {
                 }
                 body += "<br><br>\
                 <a href=\"http://m.me/338444737555231\" target=\"_blank\"><button type=\"button\" class=\"btn btn-outline-primary\">Send message to buy</button></a>\
-            </div>";
+                </div>";
                 document.getElementById("feed").innerHTML += body;
             }
         }
