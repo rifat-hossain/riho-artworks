7ee8ba655dc2e590e5e3a0d3853e05a269dc3ea0
rifat-hossain <rifathossainayon@gmail.com>
2021-02-17 10:38:57 +0600

    added timer

--- available.js
+++ available.js
@@ -2,13 +2,13 @@ var xhttp = new XMLHttpRequest();
 xhttp.open("GET", "data.json", true);
 xhttp.send(null);
 
-var nothing = true;
-var url = new URL(window.location.href);
-var q = decodeURIComponent(url.searchParams.get('q'));
-var low = parseInt(url.searchParams.get('low'));
-var high = parseInt(url.searchParams.get('high'));
 
 xhttp.onreadystatechange = function() {
+    var nothing = true;
+    var url = new URL(window.location.href);
+    var q = decodeURIComponent(url.searchParams.get('q'));
+    var low = parseInt(url.searchParams.get('low'));
+    var high = parseInt(url.searchParams.get('high'));
     if (this.readyState == 4 && this.status == 200) {
         var json = JSON.parse(xhttp.response).posts;
         for(var i=0; i < json.length; i++){
@@ -57,7 +57,21 @@ xhttp.onreadystatechange = function() {
             }
         }
         if(nothing == true){
-            document.getElementById("feed").innerHTML = "<div class=\"box text-center\"><h4>Nothing to show!</h4></div>";
+            document.getElementById("feed").innerHTML = "<div class=\"box text-center\"><h4>Oops!</h4>No artworks are currently available.<br><small>Redirecting you to Sold Artworks section in <b id=\"sec\">5</b> seconds.</small></div>";
+            var timeLeft = 4;
+            var elem = document.getElementById('sec');
+    
+            var timerId = setInterval(countdown, 1000);
+    
+            function countdown() {
+                if (timeLeft == -1) {
+                    clearTimeout(timerId);
+                    window.location.href = "sold.html";
+                } else {
+                    elem.innerHTML = timeLeft;
+                    timeLeft--;
+                }
+            }
         }
     }
 };
